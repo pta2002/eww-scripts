2d67fa0580a071f27a42e920ba5776366c62fe2c
Pedro Alves <pta2002@pta2002.com>
2022-09-14 13:26:20 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      with pkgs;{
+        devShell = mkShell
+          {
+            name = "cg";
+            buildInputs = with python3Packages; [
+              python3
+              dbus-python
+              python3Packages.pygobject3
+            ];
+          };
+        packages.nm-follow = stdenv.mkDerivation rec {
+          name = "nm-follow";
+          src = ./.;
+          propagatedBuildInputs = with python3Packages; [
+            dbus-python
+            pygobject3
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./nm-follow $out/bin/nm-follow
+          '';
+        };
+      }
+    );
+}
